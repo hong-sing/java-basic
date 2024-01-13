1e32bfafdd21d58b384a95d36a6474d0e60a4941
khs <ewoking777@gmail.com>
2024-01-13 14:29:27 +0900

    섹션11. 다형성2 : 다형성 활용1

---
+++ src/poly/ex1/AnimalSoundMain.java
@@ -0,0 +1,34 @@
+package poly.ex1;
+
+public class AnimalSoundMain {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Cow cow = new Cow();
+
+//        Cow[] cowArr = {dog, cat, cow};
+
+        System.out.println("동물 소리 테스트 시작");
+        dog.sound();
+        System.out.println("동물 소리 테스트 종료");
+
+        soundCat(cat);
+        soundCow(cow);
+    }
+
+    private static void soundCat(Cat cat) {
+        System.out.println("동물 소리 테스트 시작");
+        cat.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+
+    private static void soundCow(Cow cow) {
+        System.out.println("동물 소리 테스트 시작");
+        cow.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+
+
+    //문제 : 타입이 달라서 중복 제거가 어려움
+}
